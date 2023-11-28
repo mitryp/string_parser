e4e8fcd289b78d251e68265da01bd51732f8b18f
Dmytro Popov <thedmitryp@ukr.net>
2023-11-28 23:52:46 +0000

    Exported necessary objects

--- lib/parser.dart
+++ lib/parser.dart
@@ -3,4 +3,7 @@
 /// More dartdocs go here.
 library;
 
+export 'src/errors.dart' show InfiniteParseError;
 export 'src/parser/parser.dart' show Parser, ParserBuilder, ParserFactories, ParserMatches;
+export 'src/typedefs.dart' show ParserResult, CustomParse, ParseContext;
+export 'src/utils/result_predicates.dart' show isSuccess, isFailure;
